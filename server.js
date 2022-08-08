b189e83d8ecb2c841d2bed10cd2ae88989cead47
Jan Szewczyk <36161455+QueIvan@users.noreply.github.com>
2022-08-08 21:36:32 +0200

    [HotFix] PLS WORK

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const axios = require("axios");
+const fetch = require("node-fetch");
 
 const port = process.env.PORT || 4001;
 const app = express();
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const io = socketIo(server, { cors: { orgin: "*" } });
 
 const filter = async (mac_address_in) => {
-	const result = await axios({ method: "get", url: `${process.env.API_URL}/users/get/${mac_address_in}`, responseType: "application/json" });
+	const result = await fetch(`${process.env.API_URL}/users/get/${mac_address_in}`, { method: "GET", headers: { "Content-Type": "application/json" } });
 	return result;
 };
 
@@ -28,11 +28,10 @@ io.on("connect", async (socket) => {
 		socket.emit("failed", { status: result_row.length === 0 || result_row[0]["awaiting"] ? "newUser" : "unauthorized" });
 		if (result_row.length === 0) {
 			if (socket.handshake.headers["x-name"] !== undefined && socket.handshake.headers["x-address"] !== undefined)
-				await axios({
-					method: "post",
-					url: `${process.env.API_URL}/users/`,
-					data: { name: socket.handshake.headers["x-name"], address: socket.handshake.headers["x-address"] },
-					responseType: "application/json",
+				await fetch(`${process.env.API_URL}/users/`, {
+					method: "POST",
+					body: JSON.stringify({ name: socket.handshake.headers["x-name"], address: socket.handshake.headers["x-address"] }),
+					headers: { "Content-Type": "application/json" },
 				});
 		}
 	} else {
@@ -42,12 +41,11 @@ io.on("connect", async (socket) => {
 	socket.on("open", async (data) => {
 		const dataJson = JSON.parse(data);
 		socket.to("gate").emit("open", dataJson.gate);
-		const result = await axios({ method: "get", url: `${process.env.API_URL}/users/get/${dataJson.user_mac}`, responseType: "application/json" });
-		await axios({
-			method: "post",
-			url: `${process.env.API_URL}/logs/`,
-			data: { name: result[0].name, address: dataJson.user_mac, type: dataJson.gate, date: new Date() },
-			responseType: "application/json",
+		const result = await fetch(`${process.env.API_URL}/users/get/${dataJson.user_mac}`, { method: "GET", headers: { "Content-Type": "application/json" } });
+		await fetch(`${process.env.API_URL}/logs/`, {
+			method: "POST",
+			body: JSON.stringify({ name: result[0].name, address: dataJson.user_mac, type: dataJson.gate, date: new Date() }),
+			headers: { "Content-Type": "application/json" },
 		});
 	});
 
